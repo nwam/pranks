f20842ca25572b16d4c0ed07efdfa25e8e354df8
Noah Murad <noahmurad@hotmail.com>
2016-07-09 01:46:34 -0400

    code that should work, which is untested on windows

--- KeystrokeConfirm/src/main/java/ca/nwam/v/kc/App.java
+++ KeystrokeConfirm/src/main/java/ca/nwam/v/kc/App.java
@@ -10,9 +10,50 @@ import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import lc.kra.system.keyboard.GlobalKeyboardHook;
+import lc.kra.system.keyboard.event.GlobalKeyAdapter;
+import lc.kra.system.keyboard.event.GlobalKeyEvent;
 
 public class App {
+	private static boolean run = true;
 	public static void main(String[] args) {
+				
+		//init keyboard hook
+		GlobalKeyboardHook khook;
+		try {
+			khook = new GlobalKeyboardHook();
+		} catch (UnsatisfiedLinkError e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		//set keystroke actions
+		khook.addKeyListener(new GlobalKeyAdapter() {
+			
+            @Override public void keyPressed(GlobalKeyEvent event) {
+                System.out.println(event);
+                if(event.getVirtualKeyCode()==GlobalKeyEvent.VK_ESCAPE)
+                	System.out.println(event);
+                	createPrompt();
+            }
+            
+            @Override public void keyReleased(GlobalKeyEvent event) {
+                System.out.println(event); 
+            }
+        });
+		
+		//keep the threads (keylistener) alive
+		try {
+			while(run) Thread.sleep(128); 
+		} 
+		catch(InterruptedException e) { /* nothing to do here */ 
+	    } 
+		finally { khook.shutdownHook(); 
+	    }
+		
+	}
+	
+	private static void createPrompt() {
 		
 		//set look and feel
 		try {
