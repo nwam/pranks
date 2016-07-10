a3b1d6cf9346d2b90699916066dfc9bb19669d23
Noah Murad <noahmurad@hotmail.com>
2016-07-09 20:10:18 -0700

    keep dialog on top, make a dialog for each keystroke

--- KeystrokeConfirm/src/main/java/ca/nwam/v/kc/App.java
+++ KeystrokeConfirm/src/main/java/ca/nwam/v/kc/App.java
@@ -1,10 +1,6 @@
 package ca.nwam.v.kc;
 
-/**
- * Hello world!
- *
- */
-
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
@@ -14,10 +10,41 @@ import lc.kra.system.keyboard.GlobalKeyboardHook;
 import lc.kra.system.keyboard.event.GlobalKeyAdapter;
 import lc.kra.system.keyboard.event.GlobalKeyEvent;
 
-public class App {
-	private static boolean run = true;
+public class App implements Runnable {
+
+	@Override
+	public void run() {
+		createPrompt();
+		
+	}
+	
+	private static void createPrompt() {
+		JOptionPane optionPane = new JOptionPane("Are you sure you want to input BLAH?", JOptionPane.QUESTION_MESSAGE, JOptionPane.YES_NO_OPTION);
+		JDialog dialog = optionPane.createDialog("Confirm Input");
+		dialog.setAlwaysOnTop(true);
+		dialog.setModal(true);
+		dialog.setVisible(true);
+/*	
+		//create frame
+		JFrame frame = new JFrame();
+		SwingUtilities.updateComponentTreeUI(frame);
+		
+		//display frame
+		String[] options = {"No", "Yes"};
+		JOptionPane.showOptionDialog(frame.getContentPane(),"Are you sure you want to input BLAH?","Confirm Keystroke", 0,JOptionPane.INFORMATION_MESSAGE,null,options,null);
+*/
+	}
+	
 	public static void main(String[] args) {
-				
+		
+		//set look and feel of the prompt
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+	
 		//init keyboard hook
 		GlobalKeyboardHook khook;
 		try {
@@ -29,14 +56,15 @@ public class App {
 		
 		//set keystroke actions
 		khook.addKeyListener(new GlobalKeyAdapter() {
-			
+			//press
             @Override public void keyPressed(GlobalKeyEvent event) {
                 System.out.println(event);
                 if(event.getVirtualKeyCode()==GlobalKeyEvent.VK_ESCAPE)
                 	System.out.println(event);
                 	createPrompt();
-            }
-            
+//                	(new Thread(new App())).start();
+            }  
+            //release
             @Override public void keyReleased(GlobalKeyEvent event) {
                 System.out.println(event); 
             }
@@ -44,31 +72,13 @@ public class App {
 		
 		//keep the threads (keylistener) alive
 		try {
-			while(run) Thread.sleep(128); 
+			while(true) Thread.sleep(128); 
 		} 
 		catch(InterruptedException e) { /* nothing to do here */ 
 	    } 
 		finally { khook.shutdownHook(); 
 	    }
 		
-	}
-	
-	private static void createPrompt() {
-		
-		//set look and feel
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-				| UnsupportedLookAndFeelException e) {
-			e.printStackTrace();
-		}
-		
-		//create frame
-		JFrame frame = new JFrame();
-		SwingUtilities.updateComponentTreeUI(frame);
-		
-		String[] options = {"No", "Yes"};
-		JOptionPane.showOptionDialog(frame.getContentPane(),"Are you sure you want to input BLAH?","Confirm Keystroke", 0,JOptionPane.INFORMATION_MESSAGE,null,options,null);
 	}
 	
 }
