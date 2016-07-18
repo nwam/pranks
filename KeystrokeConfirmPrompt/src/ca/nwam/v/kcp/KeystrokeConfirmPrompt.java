8c51d559b73c984e418e716dfcc05f0735ca5401
Noah Murad <noahmurad@hotmail.com>
2016-07-18 08:54:33 -0400

    maven work

--- KeystrokeConfirmPrompt/src/ca/nwam/v/kcp/KeystrokeConfirmPrompt.java
+++ KeystrokeConfirmPrompt/src/ca/nwam/v/kcp/KeystrokeConfirmPrompt.java
@@ -5,6 +5,11 @@ import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
  class KeystrokeConfirmPrompt {
@@ -19,16 +24,29 @@ import javax.swing.ImageIcon;
 		//create and display the prompt
 		JOptionPane optionPane = new JOptionPane("You just pressed the \"" + charPressed + "\" key.", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION);
 		JDialog dialog = optionPane.createDialog("Input Information");
-		System.out.println(System.getProperty("java.class.path"));
-//		Image keyboardIcon = new ImageIcon(KeystrokeConfirmPrompt.class.getResource("../img/keybaord.png")).getImage();
-//		dialog.setIconImage(keyboardIcon);;
+		
+		//set the icon
+		try {
+			dialog.setIconImage(ImageIO.read(new File("img/keyboard.png")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+//		URL iconURL = KeystrokeConfirmPrompt.class.getResource("img/keyboard.png");
+//		System.out.println(iconURL);
+//		ImageIcon icon = new ImageIcon(iconURL);
+//		dialog.setIconImage(icon.getImage());
+//		Image keyboardIcon = new ImageIcon(KeystrokeConfirmPrompt.class.getResource("img/keyboard.png")).getImage();
+//		dialog.setIconImage(keyboardIcon);
+		
+		//set other options to make the box annoying
 		dialog.setAlwaysOnTop(true);
 		dialog.setModal(true);
 		dialog.setVisible(true);
 	}
 	
-	public static void main(String args[]){
-		String charPressed = args[1];
+	public static void main(String args[]) {
+		String charPressed;
+		charPressed = args[1];
 		createPrompt(charPressed);
 	}
 }
