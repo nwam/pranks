3ec94b0fd19d53f4d6fd9254926df0f9f2347cb6
Noah Murad <noahmurad@hotmail.com>
2016-07-20 12:04:20 -0400

    made promt more annoying ;)

--- KeystrokeConfirmPrompt/src/ca/nwam/v/kcp/KeystrokeConfirmPrompt.java
+++ KeystrokeConfirmPrompt/src/ca/nwam/v/kcp/KeystrokeConfirmPrompt.java
@@ -4,7 +4,11 @@ import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
+import java.util.Random;
 
  class KeystrokeConfirmPrompt {
 	private static void createPrompt(String charPressed) {
@@ -16,7 +20,8 @@ import java.awt.Toolkit;
 			e.printStackTrace();
 		}
 		//create and display the prompt
-		JOptionPane optionPane = new JOptionPane("You just pressed the \"" + charPressed + "\" key.", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION);
+		String message = "You just pressed the \"" + charPressed + "\" key.";
+		JOptionPane optionPane = new JOptionPane("<html><body><div width='200px' align='center'>" + message + "</div></body></html>", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION);
 		JDialog dialog = optionPane.createDialog("Input Information");
 		
 		//set the icon
@@ -26,6 +31,16 @@ import java.awt.Toolkit;
 		//set other options to make the box annoying
 		dialog.setAlwaysOnTop(true);
 		dialog.setModal(true);
+		//set the box size as (almost) fullscreen
+		Random generator = new Random(System.nanoTime());
+		int wiggle_x = (int)(generator.nextDouble() * 366) - 366/2;
+		int wiggle_y = (int)(generator.nextDouble() * 202) - 202/2;
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		int screenWidth = gd.getDisplayMode().getWidth();
+		int screenHeight = gd.getDisplayMode().getHeight();
+		dialog.setSize(screenWidth, screenHeight);
+		dialog.setLocation(0+wiggle_x,0+wiggle_y);
+		dialog.setResizable(false);
 		dialog.setVisible(true);
 	}
 	
